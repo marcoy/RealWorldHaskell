cbc21f84517491caf7885ab5e3b1590b1c6c1a7b
Marco Yuen <myuen@dstillery.com>
2014-07-02 17:55:54 -0400

    Chapter 4

---
+++ src/Ch04/InteractWith.hs
@@ -0,0 +1,16 @@
+module Ch04.InteractWith where
+
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+        input <- readFile inputFile
+        writeFile outputFile (function input)
+
+main :: IO ()
+main = mainWith myFunction
+    where mainWith function = do
+            args <- getArgs
+            case args of
+                [input, output] -> interactWith function input output
+                _ -> putStrLn "error: exactly two arguments needed"
+          myFunction = id
