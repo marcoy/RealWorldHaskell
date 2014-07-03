0da05fd0d489fd74428581156b1dec4c0c5b0c1c
Marco Yuen <myuen@dstillery.com>
2014-07-03 11:54:07 -0400

    Chapter 05

---
+++ src/Ch05/SimpleJson.hs
@@ -0,0 +1,9 @@
+module Ch05.SimpleJson where
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+            deriving (Eq, Ord, Show)
