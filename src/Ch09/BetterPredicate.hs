ce6d66569a2c12db348f47d459328c6c9ca93b1d
Marco Yuen <marcoy@gmail.com>
2014-07-10 21:27:34 -0400

    Ch09

--- src/Ch09/BetterPredicate.hs
+++ src/Ch09/BetterPredicate.hs
@@ -30,6 +30,24 @@ sizeP :: InfoP Integer
 sizeP _ _ (Just size) _ = size
 sizeP _ _ Nothing     _ = -1
 
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP q f k w x y z = f w x y z `q` k
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 q f g w x y z = f w x y z `q` g w x y z
+
+greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
+greaterP = liftP (>)
+lesserP = liftP (<)
+andP = liftP2 (&&)
+orP = liftP2 (||)
+
+simpleAndP :: InfoP Bool -> InfoP Bool -> InfoP Bool
+simpleAndP f g w x y z = f w x y z && g w x y z
+
+liftPath :: (FilePath -> a) -> InfoP a
+liftPath f w _ _ _ = f w
+
 -- construct a predicate
 equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
 equalP f k w x y z = f w x y z == k
@@ -39,6 +57,10 @@ getFileSize path = handle ((\_ -> return Nothing) :: IOError -> IO (Maybe Intege
     bracket (openFile path ReadMode) hClose $ \h -> do
         size <- hFileSize h
         return (Just size)
+
+-- (==?) = equalP
+-- (&&?) = andP
+-- (>?) = greaterP
 
 simpleFileSize :: FilePath -> IO Integer
 simpleFileSize path = do
