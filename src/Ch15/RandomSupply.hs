b4756d25661f713a91055afd5bb036a04b083f42
Marco Yuen <marcoy@gmail.com>
2014-07-11 22:58:59 -0400

    Ch15

---
+++ src/Ch15/RandomSupply.hs
@@ -0,0 +1,10 @@
+module Ch15.RandomSupply where
+
+import Ch15.Supply
+import System.Random hiding (next)
+
+randomsIO :: Random a => IO [a]
+randomsIO =
+        getStdRandom $ \g ->
+            let (a, b) = split g
+            in (randoms a, b)
---
+++ src/Ch15/Supply.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Ch15.Supply
+        (
+          Supply
+        , next
+        , runSupply
+        )
+    where
+
+
+import Control.Monad.State( State
+                          , get
+                          , put
+                          , runState)
+
+
+newtype Supply s a = S (State [s] a)
+                   deriving (Monad)
+
+-- instance Monad (Supply s) where
+--     return  = S . return
+--     s >>= k = S (unwrapS s >>= unwrapS . k)
+
+runSupply :: Supply s a -> [s] -> (a, [s])
+runSupply (S m) xs = runState m xs
+
+next :: Supply s (Maybe s)
+next = S $ do st <- get
+              case st of
+                  []     -> return Nothing
+                  (x:xs) -> do put xs
+                               return (Just x)
+
+unwrapS :: Supply s a -> State [s] a
+unwrapS (S s) = s
+
+showTwo :: (Show s) => Supply s String
+showTwo = do
+    a <- next
+    b <- next
+    return (show "a: " ++ show a ++ ", b: " ++ show b)
