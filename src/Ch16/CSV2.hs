3d14ddd7b11e265381521a807fadbae6654d8f51
Marco Yuen <marcoy@gmail.com>
2014-10-27 22:35:09 -0400

    Ch16

---
+++ src/Ch16/CSV2.hs
@@ -0,0 +1,11 @@
+module Ch16.CSV2 where
+
+import Text.ParserCombinators.Parsec
+
+csvFile = endBy line eol
+line = sepBy cell (char ',')
+cell = many (noneOf ",\n")
+eol = char '\n'
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
---
+++ src/Ch16/CSV6.hs
@@ -0,0 +1,15 @@
+module Ch16.CSV6 where
+
+import Text.ParserCombinators.Parsec
+
+csvFile = endBy line eol
+line = sepBy cell (char ',')
+cell = many (noneOf ",\n\r")
+
+eol =   try (string "\n\r")
+    <|> try (string "\r\n")
+    <|> string "\n"
+    <|> string "\r"
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
